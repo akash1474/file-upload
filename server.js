a2f348cf8da1aa0cd79369e26c85adaf2f14ccb4
akash1474 <panditakash38@gmail.com>
2020-05-10 11:40:04 +0530

    File Name functionality

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}.${file.mimetype.split('/')[1]}`);
+    },
+});
+
 //Create a post req and sending the res after the file is uploaded
-app.post('/upload', multer({ dest: path.join(__dirname, 'uploads') }).single('files'), (req, res) => {
+app.post('/upload', multer({ storage: multerStorage }).single('files'), (req, res) => {
     console.log(req.file);
     res.status(200).json({
         status: 'success',
